517057ce7d39643701851a15a7f289d80f9132c3
akbarenrico3 <akbarenrico3@gmail.com>
2020-12-17 01:04:43 +0700

    Edit Profil

--- app/components/DetailProfil.js
+++ app/components/DetailProfil.js
@@ -1,12 +1,16 @@
-import React from "react";
-import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
+import React, {useState} from "react";
+import { StyleSheet, View, Image, TouchableOpacity, Platform } from "react-native";
 import {
   Container, Separator, Content, List, ListItem, Text,
-  Left, Right, Icon, Header, Button,} from "native-base";
+  Left, Right, Icon, Header, Form, Item, Input, Label, Button} from "native-base";
 import normalize from "react-native-normalize";
-import Foto from "../assets/icons/tuyul.jpg";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-const DetailProfil = () => {
+import Foto from "../assets/icons/tuyul.jpg";
+import Edit from "../assets/icons/Edit.png";
+import verif from "../assets/icons/verif.png";
+
+const DetailProfil = ({navigation}) => {
     return (
       <Container>
         <Header style={styles.header}>
@@ -18,97 +22,93 @@ const DetailProfil = () => {
                 <Image style={styles.fotoprofil} source={Foto} />
               </View>
           </View>
-          <Separator style={styles.backgroundcolor}>
-            <Text style={styles.baseText}>
-              <Text style={styles.titleText}>Sunting Akun</Text>
-            </Text>
-          </Separator>
+          <Image style={styles.edit} source={Edit} />
+          <Form>
+            <Item stackedLabel>
+              <Label style={styles.titleText}>Nama</Label>
+              <Input textContentType="name"/>
+            </Item>
+            <Item stackedLabel>
+              <Label style={styles.titleText}>Tanggal Lahir</Label>
+              <Input textContentType="name" on/>
+            </Item>
+          </Form>
           <List>
             <ListItem>
-              <Left>
-                <Text style={styles.isiText}>Profil</Text>
-              </Left>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
+            <Left>
+                <View style={styles.flex1}>
+                    <Text style={styles.titleText}>Jenis Kelamin</Text>
+                    <Text style={styles.isiText}>Perempuan</Text>
+                </View>
+            </Left>
             </ListItem>
             <ListItem>
-              <Left>
-                <Text style={styles.isiText}>Alamat</Text>
-              </Left>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
+            <Left>
+                <View style={styles.flex1}>
+                    <View style={styles.flex2}>
+                        <Text style={styles.titleText}>Email</Text>
+                        <Image style={styles.verifikasi} source={verif} />
+                    </View>
+                    <Text style={styles.isiText}>tianacantik@gmail.com</Text>
+                </View>
+            </Left>
             </ListItem>
             <ListItem>
-              <Left>
-                <Text style={styles.isiText}>Ubah Kata Sandi</Text>
-              </Left>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
+            <Left>
+                <View style={styles.flex1}>
+                    <View style={styles.flex2}>
+                        <Text style={styles.titleText}>No. HP</Text>
+                        <Image style={styles.verifikasi} source={verif} />
+                    </View>
+                    <Text style={styles.isiText}>+123456789123</Text>
+                </View>
+            </Left>
             </ListItem>
-          </List>
-  
-          <Text>{"\n"}</Text>
-  
-          <Separator style={styles.backgroundcolor}>
-            <Text style={styles.baseText}>
-              <Text style={styles.titleText}>Umum</Text>
-            </Text>
-          </Separator>
-          <List>
-            <ListItem>
-              <Left>
-                <Text style={styles.isiText}>Tentang Rumah Jasa</Text>
-              </Left>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
-            <ListItem>
-              <Left>
-                <Text style={styles.isiText}>Syarat dan Ketentuan</Text>
-              </Left>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
-            <ListItem>
-              <Left>
-                <Text style={styles.isiText}>Kebijakan Privasi</Text>
-              </Left>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
-            <ListItem>
-              <Left>
-                <Text style={styles.isiText}>Nilai Rumah Jasa Apps</Text>
-              </Left>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
-          </List>
+        </List>
         </Content>
+        <Button onPress={() => navigation.navigate("Profil")} style={styles.button2}>
+            <Text>Simpan</Text>
+        </Button>
       </Container>
     );
   };
 
 const styles = StyleSheet.create({
+    button2:{
+        position: 'absolute',
+        height: normalize(35),
+        width: normalize(350),
+        bottom: normalize(0),
+        justifyContent: "center",
+        alignItems: 'center',
+        borderRadius: normalize(10),
+        backgroundColor: '#e42313',
+        alignSelf:'center',
+        marginBottom: normalize(20),
+      },
+    verifikasi: {
+        marginLeft: normalize(15),
+        marginTop: normalize(0),
+        marginBottom: normalize(-10),
+    },
     baseText: {
       fontFamily: "Roboto",
     },
     titleText: {
-      fontSize: 20,
-      fontWeight: "500",
-      color: "#e42313",
-    },
-    isiText: {
-      fontFamily: "Roboto",
       fontSize: 14,
+      fontWeight: "500",
+      color: "#000",
+      alignSelf:'flex-start',
+    },
+    
+    isiText: {
+      marginTop: normalize(15),
+      fontFamily: "Roboto",
+      fontSize: 12,
       fontStyle: "normal",
+      fontWeight: "300",
+      color: "#000",
+      alignSelf:'flex-start',
     },
     backgroundcolor: {
       backgroundColor: "#fff",
@@ -135,15 +135,31 @@ const styles = StyleSheet.create({
     },
     flex: {
       flexDirection: "row",
-      height: 100,
-      marginTop: 20,
+      marginTop: normalize(20),
+      marginBottom: normalize(10),
+      alignItems:'center',
+      justifyContent:'center',
+    },
+    flex1: {
+        flexDirection: "column",
+    },
+    flex2: {
+        flexDirection: "row",
     },
     fotoprofil: {
       borderWidth: 2,
-      width: 70,
-      height: 70,
-      borderRadius: 300,
-      marginLeft: 10,
+      width: normalize(70),
+      height: normalize(70),
+      borderRadius: normalize(300),
+      marginLeft: normalize(10),
+
+    },
+    edit:{
+        width: normalize(23),
+        height: normalize(23),
+        marginLeft: normalize(210),
+        marginTop: normalize(-40),
+        marginBottom: normalize(30),
     },
     header:{
       backgroundColor:'#e42313',
